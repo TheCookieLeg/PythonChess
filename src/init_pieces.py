9bce255bd6fa94bc6f66ddd80d8e8cb9268eade3
TheCookieLegend <lucas2.olafsen@gmail.com>
2026-02-16 22:19:49 +0100

    Removed some unused methods
    The initBlackPieces and initWhitePieces functions, as they were merged into one function with a parameter

--- src/init_pieces.py
+++ src/init_pieces.py
@@ -8,24 +8,6 @@ from src.ChessPieces.queen import Queen
 from src.ChessPieces.king import King
 from src.ChessPieces.pawn import Pawn
 
-
-def initBlackPieces():
-    arr = [#Rook("black"), Rook("black"),
-                   #Knight("black"), Knight("black"),
-                   #Bishop("black"), Bishop("black"),
-                   #Queen("black"), King("black"),
-                   Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"),
-                   Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black")]
-    return arr
-def initWhitePieces():
-    arr = [#Rook("white"), Rook("white"),
-                   #Knight("white"), Knight("white"),
-                   #Bishop("white"), Bishop("white"),
-                   #Queen("white"), King("black"),
-                   Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"),
-                   Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white")]
-    return arr
-
 def initPieces(side):
     arr = [#Rook(side), Rook(side),
            #Knight(side), Knight(side),
